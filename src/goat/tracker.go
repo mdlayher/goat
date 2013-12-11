71d661ad0ec0e47aa9dd52adc47e67272b760b82
Matt Layher <mdlayher@gmail.com>
2013-12-11 13:35:07 -0500

    Initial commit of tracker.go

---
+++ src/goat/tracker.go
@@ -0,0 +1,19 @@
+package goat
+
+import (
+	"bencode"
+)
+
+// Tracker announce request
+func TrackerAnnounce(resChan chan []byte) {
+	resChan <- bencode.EncString("announce successful")
+}
+
+// Report a bencoded []byte response as specified by input string
+func TrackerError(resChan chan []byte, err string) {
+	resChan <- bencode.EncDictMap(map[string][]byte {
+		"failure reason": bencode.EncString(err),
+		"min interval": bencode.EncInt(1800),
+		"interval": bencode.EncInt(3600),
+	})
+}
