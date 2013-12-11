9e1c040d019c53b1604dfb9c70a4f953179382b4
Matt Layher <mdlayher@gmail.com>
2013-12-11 13:48:39 -0500

    Move LoadConfig into config.go

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"goat"
 	"os"
@@ -13,7 +12,7 @@ const APP = "goat"
 
 func main() {
 	// Read configuration
-	conf := load_config()
+	conf := goat.LoadConfig()
 
 	// Launch manager via goroutine
 	killChan := make(chan bool)
@@ -30,23 +29,4 @@ func main() {
 		hold := <-exitChan
 		os.Exit(hold)
 	}
-}
-
-// Load configuration
-func load_config() goat.Config {
-	// Read in JSON file
-	var conf goat.Config
-	configFile, err := os.Open("config.json")
-	read := json.NewDecoder(configFile)
-
-	// Decode JSON
-	err = read.Decode(&conf)
-	if err != nil {
-		fmt.Println(APP, ": config.json could not be read, using default configuration")
-		conf.Port = "8080"
-		conf.Http = true
-		conf.Udp = false
-	}
-
-	return conf
 }
