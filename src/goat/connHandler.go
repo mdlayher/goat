536a3b8e3b65eeeac04f7432871405543412abb9
Matt Layher <mdlayher@gmail.com>
2013-12-11 13:35:01 -0500

    Update connHandler, split tracker functions into new file

--- src/goat/connHandler.go
+++ src/goat/connHandler.go
@@ -1,14 +1,13 @@
 package goat
 
 import (
-	"bencode"
 	"net"
 	"net/http"
 )
 
 // ConnHandler interface method Handle defines how to handle incoming network connections
 type ConnHandler interface {
-	Handle(l net.Listener) bool
+	Handle(l net.Listener, logChan chan string)
 }
 
 // HttpConnHandler handles incoming HTTP (TCP) network connections
@@ -16,28 +15,38 @@ type HttpConnHandler struct {
 }
 
 // Handle incoming HTTP connections and serve
-func (h HttpConnHandler) Handle(l net.Listener, logChan chan string) bool {
+func (h HttpConnHandler) Handle(l net.Listener, logChan chan string) {
+	// Set up HTTP routes for handling functions
+	http.HandleFunc("/", parseHttp)
 	http.HandleFunc("/announce", parseHttp)
 
+	// Serve HTTP requests
 	err := http.Serve(l, nil)
 	if err != nil {
 		logChan <- err.Error()
 	}
-
-	return true
 }
 
 // Parse incoming HTTP connections before making tracker calls
 func parseHttp(w http.ResponseWriter, r *http.Request) {
+	// Create channel to return bencoded response to client on
+	resChan := make(chan []byte)
+
+	// Handle tracker functions via different URLs
+	switch r.URL.Path {
+	// Tracker announce
+	case "/announce":
+		go TrackerAnnounce(resChan)
+	// Any undefined handlers
+	default:
+		go TrackerError(resChan, "Malformed announce")
+	}
+
+	// Add header to identify goat
 	w.Header().Add("Server", APP+"-git")
 
-	// Failure response for announce to wrong location
-	res := map[string][]byte {
-		"failure reason": bencode.EncBytes([]byte("Malformed announce")),
-		"min interval": bencode.EncInt(1800),
-		"interval": bencode.EncInt(3600),
-	}
-	w.Write(bencode.EncDictMap(res))
+	// Wait for response, and send it when ready
+	w.Write(<-resChan)
 }
 
 // UdpConnHandler handles incoming UDP network connections
@@ -45,6 +54,5 @@ type UdpConnHandler struct {
 }
 
 // Handle incoming UDP connections and return response
-func (u UdpConnHandler) Handle(l net.Listener) bool {
-	return true
+func (u UdpConnHandler) Handle(l net.Listener, logChan chan string) {
 }
