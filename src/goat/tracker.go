03b6ef367f35c4d44d987747836ba23a3583e329
Matt Layher <mdlayher@gmail.com>
2013-12-11 15:13:35 -0500

    Create fake announce response for demo

--- src/goat/tracker.go
+++ src/goat/tracker.go
@@ -14,7 +14,12 @@ func TrackerAnnounce(query map[string][]string, resChan chan []byte) {
 		}
 	}
 
-	resChan <- bencode.EncString("announce successful")
+	// Store announce information
+
+	// Fetch peer information
+
+	// Fake tracker announce response
+	resChan <- fakeAnnounceResponse()
 }
 
 // Report a bencoded []byte response as specified by input string
@@ -23,5 +28,28 @@ func TrackerError(resChan chan []byte, err string) {
 		"failure reason": bencode.EncString(err),
 		"min interval":   bencode.EncInt(1800),
 		"interval":       bencode.EncInt(3600),
+	})
+}
+
+// Generate a fake announce response
+func fakeAnnounceResponse() []byte {
+	return bencode.EncDictMap(map[string][]byte{
+		// min interval, interval: used to tell clients how often to announce
+		"min interval": bencode.EncInt(1800),
+		"interval":     bencode.EncInt(3600),
+		// peers: list of dictionaries
+		"peers": bencode.EncList([][]byte{
+			// peer dictionary: contains peer id, ip, and port of a peer
+			bencode.EncDictMap(map[string][]byte{
+				"peer id": bencode.EncString("ABCDEF0123456789ABCD"),
+				"ip":      bencode.EncString("127.0.0.1"),
+				"port":    bencode.EncInt(6881),
+			}),
+			bencode.EncDictMap(map[string][]byte{
+				"peer id": bencode.EncString("0123456789ABCDEF0123"),
+				"ip":      bencode.EncString("127.0.0.1"),
+				"port":    bencode.EncInt(6882),
+			}),
+		}),
 	})
 }
