614446ef01af9ab900027b2c3515d847a0a1c347
Matt Layher <mdlayher@gmail.com>
2013-12-11 00:39:13 -0500

    Make logs directory, simplify log to goat-yyyy-mm-dd.log

--- src/goat/logger.go
+++ src/goat/logger.go
@@ -9,17 +9,19 @@ import (
 )
 
 func LogMng(doneChan chan bool, logChan chan string) {
-	// create log file and pull current time to add to logfile name
-	currentTime := time.Now().String()
-	logFile, err := os.Create("GoatLog" + currentTime + ".log")
+	// Create log directory and file, and pull current date to add to logfile name
+	now := time.Now()
+	os.Mkdir("logs", os.ModeDir|os.ModePerm)
+	logFile, err := os.Create(fmt.Sprintf("logs/goat-%d-%d-%d.log", now.Year(), now.Month(), now.Day()))
 	if err != nil {
 		fmt.Println(err)
 	}
-	writer := bufio.NewWriter(logFile)
+
 	// create a logger that will use the writer created above
-	logger := log.New(writer, "", log.Lmicroseconds|log.Lshortfile)
+	logger := log.New(bufio.NewWriter(logFile), "", log.Lmicroseconds|log.Lshortfile)
 	amIDone := false
 	msg := ""
+
 	// wait for errer to be passed on the logChan channel or the done chan
 	for !amIDone {
 		select {
