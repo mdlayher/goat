35a21eec968d7a7a325a70ba42f30cb32e9f4102
Matt Layher <mdlayher@gmail.com>
2013-12-10 22:25:42 -0500

    Add basic HTTP read/write over socket

--- src/goat/connHandler.go
+++ src/goat/connHandler.go
@@ -1,7 +1,9 @@
 package goat
 
 import (
+	"fmt"
 	"net"
+	"strings"
 )
 
 // ConnHandler interface method Handle defines how to handle incoming network connections
@@ -13,7 +15,26 @@ type ConnHandler interface {
 type HttpConnHandler struct {
 }
 
+// Handle an incoming HTTP request and provide a HTTP response
 func (h HttpConnHandler) Handle(c net.Conn) bool {
+	// Read in data from socket
+	var buf = make([]byte, 1024)
+	c.Read(buf)
+
+	// TODO: remove temporary printing and fake response
+	fmt.Println("http: ", string(buf))
+	res := []string {
+		"HTTP/1.1 200 OK\r\n",
+		"Content-Type: text/plain\r\n",
+		"Content-Length: 4\r\n",
+		"Connection: close\r\n\r\n",
+		"goat\r\n",
+	}
+
+	// Write response
+	c.Write(strings.Join(res, ""))
+	c.Close()
+
 	return true
 }
 
