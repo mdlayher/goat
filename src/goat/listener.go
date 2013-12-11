73ed33147d79178a0e4a3bbf55ab15e4385c3fc7
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2013-12-10 22:32:37 -0500

    changed to only need port paramiter

--- src/goat/listener.go
+++ src/goat/listener.go
@@ -5,35 +5,30 @@ import (
 	"net"
 )
 
-// Listener interface method Listen defines a network listener which accepts connections
 type Listener interface {
-	Listen(in, out chan bool, port string)
+	Listen(port string)
 }
-
-// HttpListener listens for HTTP (TCP) connections
 type HttpListener struct {
 }
 
-// Listen on specified TCP port, accept and handle connections
-func (h HttpListener) Listen(in, out chan bool, port string) {
-	l, err := net.Listen("tcp", port)
+func (h HttpListener) Listen(port string) {
+	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		fmt.Println(err)
 	}
 	for {
 		conn, err := l.Accept()
+		handle := new(HttpConnHandler)
 		if err != nil {
 			fmt.Println(err)
 		}
-		go new (HttpConnHandler).Handle(conn)
+		go handle.Handle(conn)
 	}
 }
 
-// UdpListener listens for UDP connections
 type UdpListener struct {
 }
 
-// Listen on specified UDP port, accept and handle connections
-func (u UdpListener) Listen(in, out chan bool) {
+func (u UdpListener) Listen(port string) {
 
 }
