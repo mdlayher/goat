db36004e180806ac7e4943f5c535e305e3d9af19
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2013-12-11 23:02:47 -0500

    added better documentation

--- src/goat/dbHandler.go
+++ src/goat/dbHandler.go
@@ -1,27 +1,35 @@
 package goat
 
+//holds information for request from database
 type Request struct {
 	Query        string
 	ResponseChan chan Response
 }
+
+//holds information for response from databaseS
 type Response struct {
 	Data, Id string
 }
 
+// DbHandler interface method HandleDb defines a database handler which handles requests
 type DbHandler interface {
 	HandleDb(logChan chan string)
 }
 
+// MapDb is a key value storage database
 type MapDb struct {
 }
 
+// Handle data MapDb requests
 func (m MapDb) HandleDb(logChan chan string) {
 
 }
 
+// SqlDb is a sql based database
 type SqlDb struct {
 }
 
+// Handle Sql based requests
 func (s SqlDb) HandleDb(logChan chan string) {
 
 }
