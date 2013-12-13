bef61ec475c7cc1bdfa0cfc557816804b9b11059
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2013-12-12 23:43:52 -0500

    initial add of DbManager

--- src/goat/dbHandler.go
+++ src/goat/dbHandler.go
@@ -1,33 +1,47 @@
 package goat
 
+func DbManager(RequestChan chan Request) {
+	if Static.Config.Map {
+		go new(MapDb).HandleDb()
+		Static.LogChan <- "MapDb instance launched"
+	}
+	if Static.Config.Sql {
+		go new(SqlDb).HandleDb()
+		Static.LogChan <- "SqlDb instance launched"
+	}
+	for {
+
+	}
+}
+
 // Holds information for request from database
 type Request struct {
 	Id           string
 	Read, Write  bool
 	Data         interface{}
 	ResponseChan chan Response
-	
 }
 
 // Holds information for response from database
 type Response struct {
-	Data, Id string
+	Id, Db string
+	Data   interface{}
 }
 
 // DbHandler interface method HandleDb defines a database handler which handles requests
 type DbHandler interface {
-	HandleDb()
+	HandleDb(RequestChan chan Request)
 }
 
 // MapDb is a key value storage database
 // Id will be an identification for sharding
 type MapDb struct {
 	Id string
-	Db map[string]interface{}
+	Db map[string]map[string]interface{}
 }
 
 // Handle data MapDb requests
-func (m MapDb) HandleDb() {
+func (m MapDb) HandleDb(RequestChan chan Request) {
 
 }
 
@@ -36,6 +50,6 @@ type SqlDb struct {
 }
 
 // Handle Sql based requests
-func (s SqlDb) HandleDb() {
+func (s SqlDb) HandleDb(RequestChan chan Request) {
 
 }
