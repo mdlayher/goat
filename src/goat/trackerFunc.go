5cba6586de29b2f8e16529d996260ea3b6ddbd7f
Matt Layher <mdlayher@gmail.com>
2013-12-17 15:28:08 -0500

    Remove unused functions

--- src/goat/trackerFunc.go
+++ src/goat/trackerFunc.go
@@ -1,11 +1,8 @@
 package goat
 
 import (
-	"bencode"
-	"encoding/binary"
 	"encoding/hex"
 	"math/rand"
-	"net"
 	"strconv"
 	"time"
 )
@@ -61,47 +58,4 @@ func MapToAnnounceLog(query map[string]string) AnnounceLog {
 func RandRange(min int, max int) int {
 	rand.Seed(time.Now().Unix())
 	return min + rand.Intn(max-min)
-}
-
-// Generate a fake announce response
-func FakeAnnounceResponse(numwant int) []byte {
-	// For now, we completely ignore numwant
-	_ = numwant
-
-	res := map[string][]byte{
-		// complete, downloaded, incomplete: used to report client download statistics
-		// min interval, interval: used to tell clients how often to announce
-		// TODO: these may not be necessary, or may cause problems
-		// Need to do further research
-		/*
-			"complete":     bencode.EncInt(0),
-			"downloaded":   bencode.EncInt(0),
-			"incomplete":   bencode.EncInt(0),
-		*/
-		"interval":     bencode.EncInt(RandRange(3200, 4000)),
-		"min interval": bencode.EncInt(1800),
-		"peers":        bencode.EncBytes(CompactPeerList()),
-	}
-
-	return bencode.EncDictMap(res)
-}
-
-// Save for later: Generate a compact peer list in binary format
-func CompactPeerList() []byte {
-	// Empty byte buffer
-	buf := []byte("")
-
-	// Add a bunch of fake peers to list
-	for i := 0; i < 1; i++ {
-		// Compact peers into binary format: ip ip ip ip port port
-		ip := [4]byte{}
-		binary.BigEndian.PutUint32(ip[:], binary.BigEndian.Uint32(net.ParseIP("255.255.255.255").To4()))
-		port := [2]byte{}
-		binary.BigEndian.PutUint16(port[:], 8080)
-
-		// Append to end of buffer
-		buf = append(buf[:], append(ip[:], port[:]...)...)
-	}
-
-	return buf
 }
