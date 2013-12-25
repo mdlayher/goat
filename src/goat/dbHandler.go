381c876d1dfbff0e2c8570976ada77b5c0388f57
EliotHedeman <eliot.d.hedeman@gmail.com>
2013-12-25 01:29:09 -0500

    added init for mapdb

--- src/goat/dbHandler.go
+++ src/goat/dbHandler.go
@@ -80,8 +80,19 @@ type DbHandler interface {
 // MapDb is a key value storage database
 // Id will be an identification for sharding
 type MapDb struct {
-	Id   string
-	Busy bool
+	Id        string
+	Busy      bool
+	MapStor   map[string]map[string]interface{}
+	MapLookup map[string]*interface{}
+}
+
+func (db MapDb) init() {
+	if db.MapStor == nil {
+		db.MapStor = make(map[string]map[string]interface{})
+	}
+	if db.MapLookup == nil {
+		db.MapLookup = make(map[string]*interface{})
+	}
 }
 
 //MapDb write
