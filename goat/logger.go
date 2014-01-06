1e0b729163c3ff9c01813fd459bd668ca17db0a9
Matt Layher <mdlayher@gmail.com>
2014-01-05 20:43:39 -0500

    Remove file logging for now, as it is broken

--- goat/logger.go
+++ goat/logger.go
@@ -1,26 +1,12 @@
 package goat
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 	"time"
 )
 
 // LogManager is responsible for creating the main loggers
 func LogManager() {
-	// Create log directory and file, and pull current date to add to logfile name
-	now := time.Now()
-	os.Mkdir("logs", os.ModeDir|os.ModePerm)
-	logFile, err := os.Create(fmt.Sprintf("logs/goat-%d-%d-%d.log", now.Year(), now.Month(), now.Day()))
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// create a logger that will use the writer created above
-	logger := log.New(bufio.NewWriter(logFile), "", log.Lmicroseconds|log.Lshortfile)
-
 	// Start the system status logger
 	go StatusLogger()
 
@@ -29,9 +15,8 @@ func LogManager() {
 		select {
 		case msg := <-Static.LogChan:
 			now := time.Now()
-			log := fmt.Sprintf("%s : [%4d-%02d-%02d %02d:%02d:%02d] %s\n", App, now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), msg)
-			logger.Print(log)
-			fmt.Print(log)
+			out := fmt.Sprintf("%s : [%4d-%02d-%02d %02d:%02d:%02d] %s\n", App, now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), msg)
+			fmt.Print(out)
 		}
 	}
 }
