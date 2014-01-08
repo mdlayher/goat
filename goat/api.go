86ca2ea081e8739c528cec13e682e70a3c20a40c
Matt Layher <mdlayher@gmail.com>
2014-01-07 22:29:49 -0500

    Initial add of files API

--- goat/api.go
+++ goat/api.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
-// Any error responses from the API
+// APIError represents an error response from the API
 type APIError struct {
 	Error string `json:"error"`
 }
 
-// API router entry point
+// APIRouter handles the routing of HTTP API requests
 func APIRouter(w http.ResponseWriter, r *http.Request) {
 	// Split request path
 	urlArr := strings.Split(r.URL.Path, "/")
@@ -23,11 +24,26 @@ func APIRouter(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Check for an ID
+	ID := -1
+	if len(urlArr) == 4 {
+		i, err := strconv.Atoi(urlArr[3])
+		if err == nil {
+			ID = i
+		} else {
+			log.Println(err.Error())
+			ID = -1
+		}
+	}
+
 	// API response chan
 	apiChan := make(chan []byte)
 
 	// Choose API method
 	switch urlArr[2] {
+	// Files on tracker
+	case "files":
+		go GetFilesJSON(ID, apiChan)
 	// Server status
 	case "status":
 		go GetStatusJSON(apiChan)
@@ -43,7 +59,7 @@ func APIRouter(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-// Return an API error
+// APIErrorResponse return an APIError as JSON
 func APIErrorResponse(msg string) []byte {
 	res := APIError{
 		msg,
