c9a76191edab6c462044118bdf6ffeccbd745a3c
Matt Layher <mdlayher@gmail.com>
2014-01-10 16:46:01 -0500

    Add redis PING/PONG to validate connectivity

--- goat/redis.go
+++ goat/redis.go
@@ -14,11 +14,40 @@ func redisConnect() (redis.Conn, error) {
 // Verify that Redis server is available
 func redisPing() bool {
 	c, err := redisConnect()
+	defer c.Close()
 	if err != nil {
 		log.Println(err.Error())
 		return false
 	}
 
-	defer c.Close()
+	// Send Redis a PING to verify it is working
+	err = c.Send("PING")
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	c.Flush()
+
+	// Receive a PONG in return
+	val, err := c.Receive()
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+
+	// Ensure value is valid
+	res, err := redis.String(val, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+
+	// PONG is valid response
+	if res != "PONG" {
+		log.Println("error: redis replied to PING with:", res)
+		return false
+	}
+
+	// Redis OK
 	return true
 }
