3f755770983141f0e89d793989059b80c2239bc7
Matt Layher <mdlayher@gmail.com>
2014-02-13 23:38:03 -0500

    Initial API session handling, postLogin stub

---
+++ goat/api/login.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"github.com/mdlayher/goat/goat/data"
+)
+
+// postLogin generates a new API key for this user
+func postLogin(session data.UserRecord) ([]byte, error) {
+	return nil, nil
+}
