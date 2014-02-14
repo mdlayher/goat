064d1f5159f9deb56c3ca795ce791455e34478fe
Matt Layher <mdlayher@gmail.com>
2014-02-13 22:44:29 -0500

    Commit users schema changes for bcrypt passwords

--- res/mysql/users.sql
+++ res/mysql/users.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
 	`id` int(11) NOT NULL AUTO_INCREMENT
 	, `username` varchar(20) NOT NULL
+	, `password` char(60) NOT NULL
 	, `passkey` char(40) NOT NULL
 	, `torrent_limit` int(11) NOT NULL
 	, PRIMARY KEY (`id`)
 	, UNIQUE KEY (`username`)
+	, UNIQUE KEY (`password`)
 	, UNIQUE KEY (`passkey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
